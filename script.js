460b36d1d8337912f3b7c917f79edad68a487995
KausVlad <kausvirtual@gmail.com>
2023-04-02 10:56:33 +0300

    DOM

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const toggle = document.getElementById('toggle');
 const body = document.querySelector('body');
 
+const personalPhoto = document.querySelector('.personal-photo');
+
 toggle.addEventListener('change', function () {
   console.log(toggle.checked);
 
@@ -12,4 +14,8 @@ toggle.addEventListener('change', function () {
     body.classList.remove('my-theme');
     body.classList.add('main-theme');
   }
+});
+
+personalPhoto.addEventListener('click', function () {
+  personalPhoto.classList.toggle('personal-photo-hover');
 });
